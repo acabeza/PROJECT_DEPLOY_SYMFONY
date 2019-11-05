27a719383cd6e44d22add92b4ea55bdec8949be5
Alejandro <elavor94@gmail.com>
2019-11-05 22:29:07 +0100

    first commit for to up project

---
+++ src/Entity/Product.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $nameProduct;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $ref_product;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameProduct(): ?string
+    {
+        return $this->nameProduct;
+    }
+
+    public function setNameProduct(string $nameProduct): self
+    {
+        $this->nameProduct = $nameProduct;
+
+        return $this;
+    }
+
+    public function getRefProduct(): ?string
+    {
+        return $this->ref_product;
+    }
+
+    public function setRefProduct(?string $ref_product): self
+    {
+        $this->ref_product = $ref_product;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Users.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $ref_product;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cp;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getRefProduct(): ?string
+    {
+        return $this->ref_product;
+    }
+
+    public function setRefProduct(?string $ref_product): self
+    {
+        $this->ref_product = $ref_product;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCp(): ?int
+    {
+        return $this->cp;
+    }
+
+    public function setCp(int $cp): self
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
