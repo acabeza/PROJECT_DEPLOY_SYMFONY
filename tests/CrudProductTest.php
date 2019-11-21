fb16401efade5ef40babb53ebb0d21cd69f1fe11
Alejandro <acabeza.m.tr@gmail.com>
2019-11-21 16:48:14 +0100

    add test

---
+++ tests/CrudProductTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+
+class CrudProductTest extends WebTestCase
+{
+    private $index = "/";
+    private $edit = "/product/1/edit";
+    private $create = "/product/new";
+    private $delete = "/product/1/delete";
+    private $show = "/product/5";
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+        //Entrada al indice
+        $client->request('GET', $this->index);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testShow()
+    {
+        $client = static::createClient();
+        $client-> request('GET', $this->show);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
